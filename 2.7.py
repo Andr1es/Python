6519a3b9c5931cbfd98cb5a6de68fd78f587d66a
Andr1es <89910407+Andr1es@users.noreply.github.com>
2021-09-29 14:18:53 +0200

    Add files via upload

---
+++ 2.7.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+#loop
+random_array = np.random.rand(50)
+period = 7
+
+for i in range(period,len(random_array)):
+    array = random_array[i-period:i]
+    
+    
+#print de formule met tekst in console
+print("Gemiddelde van dag", i-period, "tot", i, "is", np.mean(array));
+                    
